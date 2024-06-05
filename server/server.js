de392f4ce27bc5cb9b157eb8941ed2a3a6e77229
VIJAY KERURE <32123795+vijaykerure@users.noreply.github.com>
2024-06-05 07:20:01 +0000

    Add mongo URI

--- server/server.js
+++ server/server.js
@@ -8,7 +8,8 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your Mongo Atlas URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb+srv://kerurevijay:cMTCKBSMTuvGJPo5@lyrical-graphql-cluster.didksql.mongodb.net/?retryWrites=true&w=majority&appName=lyrical-graphql-cluster-prod-1';
+
 if (!MONGO_URI) {
   throw new Error('You must provide a Mongo Atlas URI');
 }
